0ae601e0fcb43ca7c5a014f488a158d9dad2404c
JohnG-Dev <jtgarza48@gmail.com>
2025-10-17 19:00:42 -0700

    Started services changes to go with model implementation

--- internal/services/games.go
+++ internal/services/games.go
@@ -4,18 +4,32 @@ import (
 	"context"
 
 	"github.com/JohnG-Dev/army_builder_api/internal/database"
+	"github.com/JohnG-Dev/army_builder_api/internal/models"
 	"github.com/JohnG-Dev/army_builder_api/internal/state"
 )
 
-func GetGames(s *state.State, ctx context.Context) ([]database.Game, error) {
+func GetGames(s *state.State, ctx context.Context) ([]models.Game, error) {
 
-	games, err := s.DB.GetGames(ctx)
+	dbGames, err := s.DB.GetGames(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	if games == nil {
-		games = []database.Game{}
+	if dbGames == nil {
+		return []models.Game{}, nil
+	}
+
+	games := make([]models.Game, len(dbGames))
+	for i, g := range dbGames {
+		games[i] = models.Game{
+			ID:        g.ID,
+			Name:      g.Name,
+			Edition:   g.Edition,
+			Version:   g.Version,
+			Source:    g.Source,
+			CreatedAt: g.CreatedAt,
+			UpdatedAt: g.UpdatedAt,
+		}
 	}
 
 	return games, nil
