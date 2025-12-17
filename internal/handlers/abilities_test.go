bbc6823c1336a16575b7507aced605257249512f
JohnG-Dev <jtgarza48@gmail.com>
2025-12-16 23:59:33 -0800

    Setup final two handler tests, keywords and abilites

---
+++ internal/handlers/abilities_test.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/google/uuid"
+
+	"github.com/JohnG-Dev/army_builder_api/internal/testing"
+)
---
+++ internal/handlers/keywords_test.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+		"io"
+		"net/http/httptest"
+		"net/http"
+		"context"
+		"strings"
+		"testing"
+
+		"github.com/google/uuid"
+
+		"github.com/JohnG-Dev/army_builder_api/internal/testing"
+)
