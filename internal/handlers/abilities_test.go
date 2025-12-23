4ac1d6836dc30ecf087305792c344e141419c8ad
JohnG-Dev <jtgarza48@gmail.com>
2025-12-22 21:16:58 -0800

    Completed all handler  testing, Happy Path coverage 80% met

--- internal/handlers/abilities_test.go
+++ internal/handlers/abilities_test.go
@@ -104,6 +104,185 @@ func TestGetAbilities_FilterByUnitID(t *testing.T) {
 	}
 }
 
+func TestGetAbilities_FilterByFactionID(t *testing.T) {
+	s := setupTestDB(t)
+	ctx := context.Background()
+
+	gameID := createTestGame(t, s)
+	factionID1 := createTestFactionWithName(t, s, gameID, "Stormcast")
+	factionID2 := createTestFactionWithName(t, s, gameID, "Skaven")
+
+	_, err := s.DB.CreateAbility(ctx, database.CreateAbilityParams{
+		UnitID:    uuid.NullUUID{},
+		FactionID: database.UUIDToNullUUID(factionID1),
+		Name:      "Test Stormcast Ability",
+		Type:      "Spell",
+		Phase:     "Hero",
+		Version:   "1.0",
+		Source:    "Test Source",
+	})
+	if err != nil {
+		t.Fatalf("failed to create test stormcast ability, %v", err)
+	}
+
+	_, err = s.DB.CreateAbility(ctx, database.CreateAbilityParams{
+		UnitID:    uuid.NullUUID{},
+		FactionID: database.UUIDToNullUUID(factionID2),
+		Name:      "Test Skaven Ability",
+		Type:      "Spell",
+		Phase:     "Hero",
+		Version:   "1.0",
+		Source:    "Test Source",
+	})
+	if err != nil {
+		t.Fatalf("failed to creste test skaven ability, %v", err)
+	}
+
+	handler := &AbilitiesHandlers{S: s}
+
+	req := httptest.NewRequest(http.MethodGet, "/abilities?faction_id="+factionID1.String(), nil)
+	w := httptest.NewRecorder()
+
+	handler.GetAbilities(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status code 200, got %d", res.StatusCode)
+	}
+
+	body, _ := io.ReadAll(res.Body)
+	bodyStr := string(body)
+
+	if !strings.Contains(bodyStr, "Test Stormcast Ability") {
+		t.Errorf("expected body to contain 'test stormcast ability', got %s", bodyStr)
+	}
+
+	if strings.Contains(bodyStr, "Test Skaven Ability") {
+		t.Errorf("expected body to NOT contain 'test skaven ability', got %s", bodyStr)
+	}
+}
+
+func TestGetAbilites_FilterByType(t *testing.T) {
+	s := setupTestDB(t)
+	ctx := context.Background()
+
+	gameID := createTestGame(t, s)
+	factionID := createTestFaction(t, s, gameID)
+	unitID := createTestUnit(t, s, factionID)
+
+	_, err := s.DB.CreateAbility(ctx, database.CreateAbilityParams{
+		UnitID:    database.UUIDToNullUUID(unitID),
+		FactionID: uuid.NullUUID{},
+		Name:      "Test Stormcast Prayer",
+		Type:      "Prayer",
+		Phase:     "Hero",
+		Version:   "1.0",
+		Source:    "Test Source",
+	})
+	if err != nil {
+		t.Fatalf("failed to create test stormcast prayer, %v", err)
+	}
+
+	_, err = s.DB.CreateAbility(ctx, database.CreateAbilityParams{
+		UnitID:    database.UUIDToNullUUID(unitID),
+		FactionID: uuid.NullUUID{},
+		Name:      "Test Stormcast Passive",
+		Type:      "Passive",
+		Phase:     "End Of Turn",
+		Version:   "1.0",
+		Source:    "Test Source",
+	})
+	if err != nil {
+		t.Fatalf("failed to create test stormcast passive, %v", err)
+	}
+
+	handler := &AbilitiesHandlers{S: s}
+
+	req := httptest.NewRequest(http.MethodGet, "/abilities?type=Prayer", nil)
+	w := httptest.NewRecorder()
+
+	handler.GetAbilities(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status code 200, got %d", res.StatusCode)
+	}
+
+	body, _ := io.ReadAll(res.Body)
+	bodyStr := string(body)
+
+	if !strings.Contains(bodyStr, "Prayer") {
+		t.Errorf("expected body to contain 'spell' got %s", bodyStr)
+	}
+
+	if strings.Contains(bodyStr, "Passive") {
+		t.Errorf("expected body to NOT contain 'passive' got %s", bodyStr)
+	}
+}
+
+func TestGetAbilities_FilterByPhase(t *testing.T) {
+	s := setupTestDB(t)
+	ctx := context.Background()
+
+	gameID := createTestGame(t, s)
+	factionID := createTestFaction(t, s, gameID)
+	unitID := createTestUnit(t, s, factionID)
+
+	_, err := s.DB.CreateAbility(ctx, database.CreateAbilityParams{
+		UnitID:    database.UUIDToNullUUID(unitID),
+		FactionID: uuid.NullUUID{},
+		Name:      "Test Stormcast ability",
+		Type:      "Spell",
+		Phase:     "Hero",
+		Version:   "1.0",
+		Source:    "Test Source",
+	})
+	if err != nil {
+		t.Fatalf("failed to create test stormcast abilitiy hero, %v", err)
+	}
+
+	_, err = s.DB.CreateAbility(ctx, database.CreateAbilityParams{
+		UnitID:    database.UUIDToNullUUID(unitID),
+		FactionID: uuid.NullUUID{},
+		Name:      "Test StormCast Ability",
+		Type:      "Passive",
+		Phase:     "Shooting",
+		Version:   "1.0",
+		Source:    "Test Source",
+	})
+	if err != nil {
+		t.Fatalf("failed to create test stormcast ability passive, %v", err)
+	}
+
+	handler := &AbilitiesHandlers{S: s}
+
+	req := httptest.NewRequest(http.MethodGet, "/abilities?phase=Hero", nil)
+	w := httptest.NewRecorder()
+	handler.GetAbilities(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		t.Errorf("expected status code 200, got %d", res.StatusCode)
+	}
+
+	body, _ := io.ReadAll(res.Body)
+	bodyStr := string(body)
+
+	if !strings.Contains(bodyStr, "Hero") {
+		t.Errorf("exected body to contain 'hero', got %s", bodyStr)
+	}
+
+	if strings.Contains(bodyStr, "Shooting") {
+		t.Errorf("expected body to NOT contain 'shooting', got %s", bodyStr)
+	}
+}
+
 func TestGetAbilityByID_Success(t *testing.T) {
 	s := setupTestDB(t)
 
