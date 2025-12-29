fb54f5b8f6aefefda1d27f2319dd5e15e99c909d
JohnG-Dev <jtgarza48@gmail.com>
2025-12-28 21:02:42 -0800

    bug fixes for abilities test

--- internal/services/abilities_test.go
+++ internal/services/abilities_test.go
@@ -24,7 +24,7 @@ func TestGetAbilitiesForUnit(t *testing.T) {
 	}
 	abilities2, err := GetAbilitiesForUnit(s, ctx, unitID2)
 	if err != nil {
-		t.Fatalf("failed to get abilities 2: %v, err")
+		t.Fatalf("failed to get abilities 2: %v", err)
 	}
 
 	if len(abilities1) != 2 {
@@ -84,27 +84,47 @@ func TestGetAbilityByID_WithEffects(t *testing.T) {
 		t.Fatalf("failted to get ability: %v", err)
 	}
 
+	if ability.Effects == nil {
+		t.Fatalf("failed to initalize ability effects")
+	}
+
+	if ability.UnitID == nil || *ability.UnitID != unitID {
+		t.Errorf("ability lost its unit id pointer during the fetch")
+	}
+
 	if len(ability.Effects) != 2 {
 		t.Errorf("expected 2 ability effects, got %d", len(ability.Effects))
 	}
 
-	if ability.Effects == nil {
-		t.Errorf("failed to initalize ability effects")
+	foundSave := false
+	foundRend := false
+
+	for _, eff := range ability.Effects {
+		if eff.AbilityID != abilityID {
+			t.Errorf("effect %v is linked to wrong ability: expected %v, got %v", eff.ID, abilityID, eff.AbilityID)
+		}
+
+		switch eff.Stat {
+		case "Save":
+			foundSave = true
+			if eff.Modifier != 1 {
+				t.Errorf("Save modifier: expected 1, got %d", eff.Modifier)
+			}
+		case "Rend":
+			foundRend = true
+			if eff.Modifier != 1 {
+				t.Errorf("Rend modifier: expected 1, got %d", eff.Modifier)
+			}
+		default:
+			t.Errorf("found unexpected stat in effects: %s", eff.Stat)
+		}
 	}
 
-	if ability.Effects[0].AbilityID != abilityID {
-		t.Errorf("effect not properly linked to parent ability")
+	if !foundSave {
+		t.Errorf("did not find 'Save' effect in result")
 	}
 
-	if ability.UnitID == nil || *ability.UnitID != abilityID {
-		t.Errorf("ability lost its unit id pointer during the fetch")
-	}
-
-	if ability.Effects[0].Stat != "Save" {
-		t.Errorf("expected index 0 to be 'save', got %s", ability.Effects[0].Stat)
-	}
-
-	if ability.Effects[1].Stat != "Rend" {
-		t.Errorf("expected index 1 to be 'rend', got %s", ability.Effects[1].Stat)
+	if !foundRend {
+		t.Errorf("did not find 'Rend' effect in result")
 	}
 }
